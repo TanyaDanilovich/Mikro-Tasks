f3a4973c081f89d2a3ae2f615cc66234ae533e0b
TanyaDanilovich <tan.danilovich@gmail.com>
2022-10-16 16:14:36 +0300

    from lecture

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,60 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type FilterType = "all" | "Dollars" | "RUBLS"
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [money, setMoney] = React.useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+    const [filter, setFilter] = React.useState<FilterType>("all")
+
+
+    let currentMoney = money
+    if (filter === "Dollars") {
+        currentMoney = money.filter(el => el.banknots === "Dollars")
+    }
+    if (filter === "RUBLS") {
+        currentMoney = money.filter(el => el.banknots === "RUBLS")
+    }
+
+    const onClickFilterHandler = (valName: FilterType) => {
+        setFilter(valName)
+        console.log(filter)
+    }
+
+
+    return (
+        <div className="App">
+            <ul>
+                {currentMoney.map((el, index) => {
+                    return <li key={index}>
+                        <span>{`${el.banknots} по ${el.value}`} </span>
+                    </li>
+                })}
+            </ul>
+            <button onClick={() => {
+                onClickFilterHandler("all")
+            }}>all
+            </button>
+            <button onClick={() => {
+                onClickFilterHandler("Dollars")
+            }}>Dollars
+            </button>
+            <button onClick={() => {
+                onClickFilterHandler("RUBLS")
+            }}>RUBLS
+            </button>
+        </div>
+    );
 }
 
 export default App;
